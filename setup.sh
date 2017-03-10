b6bea65d4375557af5d53c429b22a3c198a123bb
JP-Ellis <josh@jpellis.me>
2017-03-10 16:11:02 +1100

    Another big re-write of the setup script
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
+
+setopt NULL_GLOB
 
 help () {
     echo "Usage: $1"
@@ -9,7 +11,7 @@ help () {
     echo "made with the extension \`.bak.$RANDOM\`."
 }
 
-if [[ $# -gt 0 ]]; then
+if [ $# -gt 0 ]; then
     help $0
     exit 1
 fi
@@ -19,10 +21,10 @@ fi
 # yes_or_no(<statement>, <default>)
 yes_or_no() {
     while true; do
-        if [ "${2:-}" = "Y" ]; then
+        if [ "${2:-}" -eq "Y" ]; then
             prompt="Y/n"
             default=Y
-        elif [ "${2:-}" = "N" ]; then
+        elif [ "${2:-}" -eq "N" ]; then
             prompt="y/N"
             default=N
         else
@@ -30,7 +32,7 @@ yes_or_no() {
             default=
         fi
 
-        REPLY=
+        unset REPLY
         local compcontext='yn:yes or no:(y n)'
         vared -cp "$1 [$prompt] " REPLY
 
@@ -45,72 +47,57 @@ yes_or_no() {
     done
 }
 
-## Miscellaneous individual files
+make_backup() {
+    if [ $# -ne 1 ]; then
+        echo "$0 requires exactly one argument." >&2
+        exit 1
+    fi
+    if [ -e "$1" ]; then
+        printf "Moved "
+        mv -v "$1" "$1.bak.$RANDOM"
+    fi
+}
+
+make_link() {
+    if [ $# -ne 2 ]; then
+        echo "$0 requires exactly two arguments." >&2
+        exit 1
+    fi
+    make_backup "$2"
+    printf "Linked "
+    ln -vs "$1" "$2"
+}
+
+## Link files and directories
 ################################################################################
 
 ## Files in the home directory
-for file in ".dir_colors" ".latexmkrc" ".profile" ".spacemacs" ".Xresources"; do
+for f in $(pwd)/home/* $(pwd)/home/.*; do
+    f="$(basename "$f")"
     yes_or_no "Link $file into the home directory?" Y || continue
-
-    if [[ -e "$HOME/$file" ]]; then
-        mv -v "$HOME/$file" "$HOME/$file.bak.$RANDOM"
-    fi
-    ln -fvs "$(pwd)/home/$file" "$HOME/$file"
+    make_link "$(pwd)/home/$file" "$HOME/$file"
 done
 
-## Files in the config directory
-for file in "pgfplots.default.tex"; do
-    yes_or_no "Link $file into the config directory?" Y || continue
-
-    if [[ -e "$HOME/.config/$file" ]]; then
-        mv -v "$HOME/.config/$file" "$HOME/.config/$file.bak.$RANDOM"
-    fi
-    ln -fvs "$(pwd)/config/$file" "$HOME/.config/$file"
+mkdir -p "$HOME/.config"
+for f in $(pwd)/config/* $(pwd)/config/.*; do
+    f="$(basename "$f")"
+    yes_or_no "Link $file into the .config directory?" Y || continue
+    make_link "$(pwd)/config/$file" "$HOME/.config/$file"
 done
-
-
-## Configuration directories
-################################################################################
-
-for dir in $(pwd)/config/*/(.N); do
-    dir="${${dir#$(pwd)/config/}%/}"
-    yes_or_no "Setup $dir" Y || continue
-    if [[ -e "$HOME/.config/$dir" ]]; then
-        mv -v "$HOME/.config/$dir" "$HOME/.config/$dir.bak.$RANDOM"
-    fi
-    ln -fvs "$(pwd)/config/$dir" "$HOME/.config/$dir.bak.$RANDOM"
-done
-
-for dir in $(pwd)/home/*/(.N) $(pwd)/home/.*/(.N); do
-    dir="${${dir#$(pwd)/home/}%/}"
-    yes_or_no "Setup $dir" Y || continue
-    if [[ -e "$HOME/$dir" ]]; then
-        mv -v "$HOME/$dir" "$HOME/$dir.bak.$RANDOM"
-    fi
-    ln -fvs "$(pwd)/home/$dir" "$HOME/$dir"
-done
-
-
-## Scripts
-################################################################################
 
 mkdir -p "$HOME/.local/bin"
-for bin in $(pwd)/bin/*(.N); do
-    bin="${bin#$(pwd)/bin/}"
-    yes_or_no "Link $bin ?" Y || continue
-    if [[ -e "$HOME/.local/bin/$bin" ]]; then
-        mv -v "$HOME/.local/bin/$bin" "$HOME/.local/bin/$bin.bak.$RANDOM"
-    fi
-    ln -fvs "$(pwd)/bin/$bin" "$HOME/.local/bin/$bin"
+for f in $(pwd)/bin/* $(pwd)/bin/.*; do
+    f="$(basename "$f")"
+    yes_or_no "Link $file into .local/bin" Y || continue
+    make_link "$(pwd)/bin/$file" "$HOME/.local/bin/$file"
 done
-
 
 ## ZSH
 ################################################################################
 
 if yes_or_no "Setup ZSH?" Y; then
     # Clone (or update) ZSH
-    if [[ ! -d "$HOME/.zprezto" ]]; then
+    if [ ! -d "$HOME/.zprezto" ]; then
         echo "Cloning zprezto"
         git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
         ln -fvs "$(pwd)/prompt_jpellis_setup" "${ZDOTDIR:-$HOME}/.zprezto/modules/prompt/functions/prompt_jpellis_setup"
@@ -122,14 +109,6 @@ if yes_or_no "Setup ZSH?" Y; then
     # Make the DEFAULT_USER the current user
     sed -i "s#DEFAULT_USER=\"josh\"#DEFAULT_USER=\"$(whoami)\"#" "$(pwd)/home/.zshrc"
 
-    # Link the files
-    for file in ".zlogin" ".zlogout" ".zpreztorc" ".zprofile" ".zshenv" ".zshrc"; do
-        if [[ -e "$HOME/$file" ]]; then
-            mv -v "$HOME/$file" "$HOME/$file.bak.$RANDOM"
-        fi
-        ln -fvs  "$(pwd)/home/$file" "$HOME/$file"
-    done
-
     yes_or_no "Make ZSH the default shell?" Y && chsh -s $(which zsh) $(whoami)
 fi
 
@@ -139,17 +118,11 @@ fi
 
 if yes_or_no "Setup Spacemacs?" Y; then
     # Clone spacemacs if it hasn't been done already.
-    if [[ ! -d "$HOME/.emacs.d" ]]; then
+    if [ ! -d "$HOME/.emacs.d" ]; then
         git clone --branch develop https://github.com/syl20bnr/spacemacs "$HOME/.emacs.d"
     else
         mv "$HOME/.emacs.d" "$HOME/.emacs.d.bak.$RANDOM"
         mv "$HOME/.emacs" "$HOME/.emacs.bak.$RANDOM" 2>/dev/null
         git clone --branch develop https://github.com/syl20bnr/spacemacs ~/.emacs.d
     fi
-
-    # Link the files
-    if [[ -e "$HOME/.spacemacs" ]]; then
-        mv -v "$HOME/.spacemacs" "$HOME/.spacemacs.bak.$RANDOM"
-    fi
-    ln -fvs "$(pwd)/home/.spacemacs" "$HOME/.spacemacs"
 fi
