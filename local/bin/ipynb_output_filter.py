bb7f008857aad2efae1ae97a2174a0b1cf6f465b
JP-Ellis <josh@jpellis.me>
2019-07-05 14:10:34 +0800

    Update ipynb formatting

--- local/bin/ipynb_output_filter.py
+++ local/bin/ipynb_output_filter.py
@@ -6,18 +6,19 @@
 import sys
 
 try:
-    runarg_idx = sys.argv.index('--rundir')
+    runarg_idx = sys.argv.index("--rundir")
     rundir = sys.argv[runarg_idx + 1]
     import os
+
     os.chdir(os.path.expanduser(rundir))
 except ValueError:
     pass
 
 version = None
 
-if sys.version[0] == '2':
+if sys.version[0] == "2":
     reload(sys)
-    sys.setdefaultencoding('utf8')
+    sys.setdefaultencoding("utf8")
 
 try:
     # Jupyter
@@ -29,18 +30,20 @@ except ImportError:
     except ImportError:
         # Old IPython
         from IPython.nbformat.current import reads, write
-        version = 'json'
+
+        version = "json"
 
 to_parse = sys.stdin.read()
 
 if not version:
     import json
+
     json_in = json.loads(to_parse)
-    version = json_in['nbformat']
+    version = json_in["nbformat"]
 
 json_in = reads(to_parse, version)
 
-if hasattr(json_in, 'worksheets'):
+if hasattr(json_in, "worksheets"):
     # IPython
     sheets = json_in.worksheets
 else:
@@ -51,10 +54,10 @@ for sheet in sheets:
     for cell in sheet.cells:
         if "outputs" in cell:
             cell.outputs = []
-        for field in ("prompt_number", "execution_number"):
+        for field in ["prompt_number", "execution_number"]:
             if field in cell:
                 del cell[field]
-        for field in ("execution_count", ):
+        for field in ["execution_count"]:
             if field in cell:
                 cell[field] = None
 
@@ -70,7 +73,7 @@ for sheet in sheets:
         if hasattr(sheet.metadata.language_info, "version"):
             del sheet.metadata.language_info["version"]
 
-if 'signature' in json_in.metadata:
-    json_in.metadata['signature'] = ""
+if "signature" in json_in.metadata:
+    json_in.metadata["signature"] = ""
 
 write(json_in, sys.stdout, version)
