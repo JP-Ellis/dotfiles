6e2869aa565588a0bb80153b526268fc8c0ef61c
Josh Ellis <jellis6@kpmg.com.au>
2022-08-11 16:25:22 +1000

    [doom] Add org-roam-ui
    Signed-off-by: Josh Ellis <jellis6@kpmg.com.au>

--- doom/packages.el
+++ doom/packages.el
@@ -5,5 +5,8 @@
 (package! copilot
   :recipe (:host github :repo "zerolfx/copilot.el" :files ("dist" "*.el")))
 
+(unpin! org-roam)
+(package! org-roam-ui)
+
 (package! xah-wolfram-mode
   :recipe (:host github :repo "xahlee/xah-wolfram-mode"))
